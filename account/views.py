3585a3e708d342c34eb6c9de70c638686707997b
toughtiger <santosh.cerdak@gmail.com>
2021-05-03 15:21:11 +0530

    added alerts

--- account/views.py
+++ account/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import redirect, render
 
+def register(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'accounts/register.html')
+
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
 
-def register(request):
-    return render(request, 'accounts/register.html')
 
 
 def dashboard(request):
+    
     return render(request, 'accounts/dashboard.html')
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
@@ -14,11 +14,14 @@ def index(request):
 
 
 def listing(request, listing_id):
-    listing = Listing.objects.filter(id = listing_id)
+    listing = get_object_or_404(Listing, pk=listing_id)
+   
     context = {
         'listing': listing
     }
     return render(request, 'listings/listing.html', context)
+
+
 
 def search(request):
     return render(request, 'listings/search.hrml')
